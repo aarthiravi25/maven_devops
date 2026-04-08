bee4913544a87ae35ccb8d9ede02dec7914defde
Aarthi <aarthi.27csa@licet.ac.in>
2026-04-08 15:13:42 +0530

    inital commit

---
+++ src/main/java/com/example/demo/OrderController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class OrderController {
+
+    @GetMapping("/")
+    public String showForm(Model model) {
+        model.addAttribute("user", new User());
+        return "index";
+    }
+
+    @PostMapping("/order")
+    public String placeOrder(@ModelAttribute User user, Model model) {
+        String message = "Order placed successfully! Item: " 
+                + user.getItemName() + ", Quantity: " 
+                + user.getQuantity();
+
+        model.addAttribute("message", message);
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+public class User {
+    private String itemName;
+    private int quantity;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
